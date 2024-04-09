63e993afc6fbeb97ce4b7fe9fea5ccf9b485ca29
maasam514 <mohammadsam.karimi.1@ens.etsmtl.ca>
2024-04-08 21:53:48 -0400

    first

--- app/src/main/java/Vue/EcranAcceuil.java
+++ app/src/main/java/Vue/EcranAcceuil.java
@@ -1,6 +1,9 @@
 package Vue;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.mastermind.R;
 
-public class EcranAcceuil extends AppCompatActivity {
+public class EcranAcceuil extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ecran_acceuil);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        Button btnJouer = findViewById(R.id.btnJouer);
+        btnJouer.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent versJeu = new Intent(this,EcranJeu.class);
+        startActivity(versJeu);
     }
 }
